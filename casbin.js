780193668890f21eb3dbd0930b0fecf6327f3eea
Mikolaj Figurski <miko.f80@gmail.com>
2024-04-25 13:42:20 -0400

    write casbin role adaptor

--- casbin.js
+++ casbin.js
@@ -1,21 +1,45 @@
 import { App } from '@tinyhttp/app';
-import { newEnforcer } from 'casbin';
+import { newEnforcer, Helper } from 'casbin';
 import { authenticate, reply, roleDB } from './userSession.js';
 
-const enforcer = await newEnforcer('./casbin_models/rbac.conf', './casbin_models/roles.csv', );
+const policies = {
+  user: ['case:list', 'case:read'],
+  admin: ['case:list', 'case:read', 'case:create', 'case:update'],
+};
+const casbinAdapter = {
+  loadPolicy: async (model) => {
+    // load `policies`
+    Object.entries(policies).forEach(([role, perms]) => {
+      perms.forEach(perm => {
+        const [obj, act] = perm.split(':');
+        Helper.loadPolicyLine(`p, ${role}, ${obj}, ${act}`, model);
+      });
+    });
+    // load roles
+    const roles = roleDB.JSON();
+    Object.entries(roles).forEach(([user, role]) => {
+      Helper.loadPolicyLine(`g, ${user}, ${role}`, model);
+    });
+  },
+};
+const enforcer = await newEnforcer('casbin_models/rbac.conf', casbinAdapter);
 
 const app = new App();
 app.get('casbin/', async (req, res) => {
   const usr = authenticate(req);
   if (!usr) return reply.unauthed(res);
-  const roles = enforcer.getRolesForUser(usr);
-  return res.send('<h3>Hello ${usr}, from Casbin!</h3>');
+  return res.send(`<h3>Hello ${usr}, from Casbin!</h3>`);
+});
+app.get('casbin/debug', async (_, res) => {
+  const dump = enforcer.getAllSubjects();
+  return res.json(dump);
 });
 app.get('casbin/case', async (req, res) => {
   const usr = authenticate(req);
   if (!usr) return reply.unauthed(res);
-  const can = await enforcer.enforce(usr, 'case', 'list');
+  const [can, explain] = await enforcer.enforceEx(usr, 'case', 'list');
   if (!can) return reply.forbidden(res);
+  console.debug('Passed auth:', explain);
   return res.send('<h3>Youve listed cases!</h3>');
 });
 app.post('casbin/case', async (req, res) => {
