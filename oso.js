782fb4e1a48e51ce4150a63a08aabf3f96fe03eb
Mikolaj Figurski <miko.f80@gmail.com>
2024-04-26 15:32:23 -0400

    oso role sync function

--- oso.js
+++ oso.js
@@ -18,16 +18,32 @@ import { authenticate, reply, roleDB } from './userSession.js';
  *   "delete" if "owner";
  * }
  */
+
 const API_KEY="e_3wx6VCK6WqKwtk8hwyTSk7_EtayGNmShYu_46leh3dv7D9mUqWxXkVLzp3HPI1m"
 const oso = new Oso("https://cloud.osohq.com", API_KEY);
+async function oso_sync() {
+  const role_assignments = roleDB.JSON();
+  let role_facts = [];
+  Object.entries(role_assignments).forEach(([user, roles]) => {
+    roles.forEach(role =>
+      role_facts.push(['has_role', { type: 'User', id: user }, role])
+    );
+  });
+  console.debug('[oso] syncing roles:', role_facts);
+  await oso.bulkTell(role_facts);
+  return true;
+}
+let has_synced = false;
 
 const app = new App();
-app.get('oso/', (req, res) => {
+app.get('oso/', async (req, res) => {
+  if (!has_synced) has_synced = await oso_sync();
   const usr = authenticate(req);
   if (!usr) return reply.unauthed(res);
   res.send(`<h3>Hello ${usr} from Oso!</h3>`);
 });
 app.get('oso/case', async (req, res) => {
+  if (!has_synced) has_synced = await oso_sync();
   const usr = authenticate(req);
   if (!usr) return reply.unauthed(res);
   const actor = { type: 'User', id: usr };
@@ -37,6 +53,7 @@ app.get('oso/case', async (req, res) => {
   res.send(`<h3>Listing cases!</h3>`);
 });
 app.post('oso/case', async (req, res) => {
+  if (!has_synced) has_synced = await oso_sync();
   const usr = authenticate(req);
   if (!usr) return reply.unauthed(res);
   const actor = { type: 'User', id: usr };
@@ -45,6 +62,5 @@ app.post('oso/case', async (req, res) => {
   if (!decision) return reply.forbidden(res);
   res.send(`<h3>Creating a case!</h3>`);
 });
-
 
 export default app;
